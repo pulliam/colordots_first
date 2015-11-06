68844212166e15dad8bd0dd4eba524d0ac5cfc3e
Thais Pulliam <thaispulliam@gmail.com>
2015-11-05 22:07:04 -0500

    added shuffle code to main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var cardsArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'I', 'I', 'J', 'J'];
+
+Array.prototype.shuffleMePlease = function(){
+    var i = this.length;
+    var rand;
+    var randPosition;
+        while (--i > 0){
+            rand = Math.floor(Math.random() * (i+1));
+            randPosition = this[rand];
+            this[rand] = this[i];
+            this[i] = randPosition;
+        }
+    return this;
+}
+
+cardsArray.shuffleMePlease();
+console.log(cardsArray);
